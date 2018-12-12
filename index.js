b412ddf3b9a99313db3562dbee928347bc3267f5
Aditya Respati <dityo.hario@gmail.com>
2018-12-12 19:52:17 +0700

    commit

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
+const command = process.argv.slice(2)
+const Controller  = require('./controller.js')
+
+class Index {
+    constructor(command){
+        this.command = command
+    }
+
+     executeCommand(){
+        switch (this.command[0]){
+            case 'register': 
+                Controller.register(this.command[1], this.command[2], this.command[3])
+            break;
+            case 'login':
+                Controller.login(this.command[1], this.command[2])
+            break;
+            case 'addPatient':
+                Controller.addPatient(this.command[1], this.command[2])
+            break;
+            case 'logout':
+                Controller.logout()
+            break;
+        }
+    }
+}
+
+let action = new Index(command)
+action.executeCommand()
 
